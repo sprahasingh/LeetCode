92b117907e073704c1a7657efc10f4e65afc688f
Spraha Singh <99238925+sprahasingh@users.noreply.github.com>
2026-07-16 16:13:58 +0530

    Time: 0 ms (100%), Space: 9.3 MB (20.57%) - LeetHub

---
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long sumAndMultiply(int n) {
+        int num = n;
+        int sum = 0;
+        string s = "";
+        while(num){
+            int d = num % 10;
+            sum += d;
+            if(d >= 1 && d <= 9) s += to_string(d);
+            num /= 10;
+        }
+        if(s.empty()) s = "0";
+        else reverse(s.begin(), s.end());
+        long long x = stoll(s);
+        return 1LL*x*sum;
+    }
+};
