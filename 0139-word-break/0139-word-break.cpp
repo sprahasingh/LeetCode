2b8acb28c35d85e065b3bcd18a1df02810df67d6
Spraha Singh <99238925+sprahasingh@users.noreply.github.com>
2026-07-19 00:41:46 +0530

    Time: 8 ms (51.32%), Space: 14.7 MB (53.36%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool solve(string s, unordered_set<string>& occ, unordered_map<string,bool>& dp){
+        if (s.empty()) return true;
+        if (dp.count(s)) return dp[s];
+        string word = "";
+        for(int i = 0; i < s.size(); ++i){
+            word += s[i];
+            if(occ.count(word)){
+                if(solve(s.substr(i + 1), occ, dp)){
+                    dp[s.substr(i + 1)] = true;
+                    return true;
+                }
+                else{
+                    dp[s.substr(i + 1)] = false;
+                }
+            }
+        }
+        return false;
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.size();
+        unordered_set<string> occ(wordDict.begin(), wordDict.end());
+        unordered_map<string,bool> dp(n+1);
+        return solve(s, occ, dp);
+    }
+};
