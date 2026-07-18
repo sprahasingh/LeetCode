aac7b34b38b3470ff12b5dfb7c7d8e375aa5b507
Spraha Singh <99238925+sprahasingh@users.noreply.github.com>
2026-07-18 18:29:41 +0530

    Time: 1273 ms (15.2%), Space: 409.7 MB (6.45%) - LeetHub

--- 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     const int MOD = 1e9 + 7;
     int solve(int idx, int gcd1, int gcd2, vector<int>& nums, vector<vector<vector<int>>>& dp){
-        if(dp[idx][gcd1][gcd2] != -1) return dp[idx][gcd1][gcd2];
         if(idx == nums.size()) return (gcd1 > 0 && gcd2 > 0 && gcd1 == gcd2);
-        
+        if(dp[idx][gcd1][gcd2] != -1) return dp[idx][gcd1][gcd2];
         int ans = 0;
         ans = (ans + solve(idx + 1, std::gcd(gcd1,nums[idx]), gcd2, nums, dp)) % MOD;
         ans = (ans + solve(idx + 1, gcd1, std::gcd(gcd2,nums[idx]), nums, dp)) % MOD;
